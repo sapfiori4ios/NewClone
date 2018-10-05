93451c23bf13382a018b97675ce515e69814a293
Basavaraja <basavaraja@admins-MacBook-Pro.local>
2018-10-05 15:41:12 +0530

    New Commit

--- NewClone/ViewController.swift
+++ NewClone/ViewController.swift
@@ -9,8 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+    // delete framework
+    override func viewDidLoad(){
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
